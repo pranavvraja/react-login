ef427f2c3a405d4571f35beacefdd90f94cf3449
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2023-08-06 20:56:56 +0530

    Added window prompts in welcome page

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -4,29 +4,33 @@ import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
 import { Navigate, useNavigate } from "react-router-dom";
 
-const welcome = () => {
+const welcome = (user) => {
   const navigate = useNavigate();
-  // const email = user.email;
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const uid = user.uid;
         const email = user.email;
-        const name = user.displayName;
+        // const name = user.displayName;
         console.log(uid);
         console.log(email);
-        console.log(name);
+        window.alert(`Welcome, ${email}`);
+
+        // console.log(name);
       } else {
         console.log("No user is signed in.");
         navigate("/");
       }
     });
   }, []);
+  const email = user.email;
 
   const logout = () => {
     signOut(auth)
       .then(() => {
         console.log("User signed out");
+        window.alert("User signed out");
         navigate("/");
       })
       .catch((error) => {
@@ -37,9 +41,8 @@ const welcome = () => {
   return (
     <>
       <div className="w-96 h-auto p-10 text-1xl justify-center flex flex-col items-start bg-slate-200 border-spacing-3 rounded-xl shadow-xl ">
-        <h1 className="text-slate-700 items-center">Welcome, </h1>
+        <h1 className="text-slate-700 items-center">{`Welcome, ${email}`} </h1>
 
-        <p>{}</p>
         <button
           onClick={logout}
           className="text-1xl text-slate-100 bg-slate-900 rounded-lg px-4 py-2 my-4"
